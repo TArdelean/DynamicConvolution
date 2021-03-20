48666b95c703b92538dcaf22f385c226b05fe5b9
Alex Larionov <alxs.larionov@gmail.com>
2021-03-20 23:59:23 +0300

    Fix ConvLayer passing as a wrong argument to the backbone

--- models/deeplab_details/backbone/__init__.py
+++ models/deeplab_details/backbone/__init__.py
@@ -2,12 +2,12 @@ from ...deeplab_details.backbone import resnet, xception, drn, mobilenet
 
 def build_backbone(backbone, output_stride, BatchNorm, ConvLayer):
     if backbone == 'resnet':
-        return resnet.ResNet101(output_stride, BatchNorm, ConvLayer)
+        return resnet.ResNet101(output_stride, BatchNorm, ConvLayer=ConvLayer)
     elif backbone == 'xception':
-        return xception.AlignedXception(output_stride, BatchNorm, ConvLayer)
+        return xception.AlignedXception(output_stride, BatchNorm, ConvLayer=ConvLayer)
     elif backbone == 'drn':
-        return drn.drn_d_54(BatchNorm, ConvLayer)
+        return drn.drn_d_54(BatchNorm, ConvLayer=ConvLayer)
     elif backbone == 'mobilenet':
-        return mobilenet.MobileNetV2(output_stride, BatchNorm, ConvLayer)
+        return mobilenet.MobileNetV2(output_stride, BatchNorm, ConvLayer=ConvLayer)
     else:
         raise NotImplementedError
