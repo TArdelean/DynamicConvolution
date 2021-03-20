4b115f23e54f63d86531c8b061b8eba5da0b1bab
Alex Larionov <alxs.larionov@gmail.com>
2021-03-21 00:10:06 +0300

    Fix missing imports for CustomSequential once again

--- models/common.py
+++ models/common.py
@@ -3,6 +3,7 @@ from torch import nn
 from torch.nn import *
 from collections import OrderedDict
 from typing import Any, Iterable, Iterator, Mapping, Optional, TYPE_CHECKING, overload, Tuple, TypeVar, Union
+T = TypeVar('T', bound=Module)
 
 class Conv2dWrapper(nn.Conv2d):
     """
